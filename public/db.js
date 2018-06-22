418f07a620fbe9b1b6891611d8d394958f528a45
katongole <katsisaac50@gmail.com>
2018-06-22 16:50:57 +0200

    AJAX request created to fetch data from the api.

--- public/db.js
+++ public/db.js
@@ -1,14 +1,23 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('SH01_json.json')
-const middlewares = jsonServer.defaults()
+// 1. try and connect to the server over XHR (XMLHTTPRequest)
+// 2. Get some data from the database
+// 3. Just console.log it
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// Maybe abstract queries to the server so you have a function
 
-console.log("connected")
-/* const data=JSON.parse("SH01_json.json")
-console.log(data) */
+function fetchData(searchString, callback) {
+  // Do XHR to server with search string
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     let response=JSON.parse(xhttp.responseText);
+     console.log(response)
+    }
+  };
+  xhttp.open("GET", "items", true);
+  xhttp.send();
+  // Call callback with parsed JSON
+}
+console.log('connected')
+
+
+// 
